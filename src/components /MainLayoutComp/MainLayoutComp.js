f6f5a92c4a814d2842e4a657f30d11085a88b0b5
John Nydam <jnydam@brandeis.edu>
2022-05-20 08:58:11 -0400

    Uploaded boilerplate project

---
+++ src/components /MainLayoutComp/MainLayoutComp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './MainLayoutComp.module.css';
+
+const MainLayoutComp = (props) => {
+
+    return (<div class={styles.mainLayoutCompContainer}>
+        <div class={styles.outerControlBarHeaderContainer}>
+
+        </div>
+        <div class={styles.mainKeyboardContainer}>
+
+        </div>
+    </div>)
+}
+
+export default MainLayoutComp;
